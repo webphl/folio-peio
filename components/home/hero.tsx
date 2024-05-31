d68af3dc7b5c2df0d9106f3b32620a92dbd0211b
WebPhL.com <102985753+webphl@users.noreply.github.com>
2024-05-31 18:26:45 +0200

    Update hero.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -82,8 +82,8 @@ const HeroSection = React.memo(() => {
   const renderHeroContent = (): React.ReactNode => (
     <div className={HERO_STYLES.CONTENT}>
       <div className="md:mb-4 mb-2">
-        <h2 className="text-4xl seq">Hello 👋🏻</h2>
-        <h1 className="text-3xl seq">I am Ayush Singh</h1>
+        <h2 className="text-4xl seq">Bonjour 👋🏻</h2>
+        <h1 className="text-3xl seq">Je suis Peio/h1>
       </div>
       <p className="mb-4">
         <span className={HERO_STYLES.TYPED_SPAN} ref={typedSpanElement}></span>
